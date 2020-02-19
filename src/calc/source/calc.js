2b03c6d923d7d09f0a40f4f114a0a87746aaee54
AlexeyLocker <alexey.locker@gmail.com>
2020-02-19 14:13:51 +0200

    Update 19.02.2020

--- src/calc/source/calc.js
+++ src/calc/source/calc.js
@@ -1,9 +1,10 @@
 "use strict"
 const equation = document.getElementById("label");
 const button = document.querySelectorAll(".number")
-let digit1;
-let digit2;
-function sum(a,b){return a + b;}
+ 
+
+
+function sum(a,b){return +a + +b;}
 
 
 
@@ -16,56 +17,69 @@ function multip(a,b){return a * b;}
 
 function division(a,b){return a / b;}
 
-
-// function valueAssign(value){
-//     let digit = 0;
-    
-//     for(let i = 0; i < value.length; i++){
-//         let num = value[i];
-//         if (num == "+"){
-//             digit = Number(digit);
-//             return sum(digit, valueAssign(value.slice(i+1)))
-//         }else if(num == "-"){
-//             return substr(digit, valueAssign(value.slice(i+1)))
-//         }else if(num == "*"){
-//             return multip(digit, valueAssign(value.slice(i+1)))
-//         }else if(num == "/"){
-//             return division(digit, valueAssign(value.slice(i+1)))
-//         } else {
-//                 digit += num;
-//             }
-        
-//     }
-//     return digit;
-// }
+let digit1;
+let digit2;
+let operator;
 
 function equally (event){
-    console.log(digit1 + "   " + digit2 + "   " + sum(digit1, equation.value));
-    switch (event.target.value){
-        case "+": 
-        (digit1 === undefined) ? digit1 = equation.value : digit2 = equation.value;
-        equation.value = ""
+    
+    if ((event.target.value === "=" && digit1 !== undefined) || digit2 !== undefined){
+        if (digit2 === undefined) digit2 = equation.value;
+        switch (operator){
+            case "+": equation.value = sum(digit1,digit2); 
+               break;
+             case "-": equation.value = substr(digit1,digit2);    
+              break;
+             case "/": equation.value = division(digit1,digit2); 
+              break;
+             default:  equation.value = multip(digit1,digit2); 
+        }
+          digit1 = undefined;
+          digit2 = undefined;
+    }else if(digit2 === undefined){
+        console.log(event)
+    
+        switch (event.target.value){
+       
+         case "+": 
+               (digit1 === undefined) ? digit1 = equation.value : digit2 = equation.value;
+               operator = "+";
+               equation.value = ""
             break;
+
         case "-": 
-        (digit1 === undefined) ? digit1 = equation.value : digit2 = equation.value;
-        equation.value = ""
+               (digit1 === undefined) ? digit1 = equation.value : digit2 = equation.value;
+               operator = "-";
+               equation.value = ""
             break;
+
         case "/": 
-        (digit1 === undefined) ? digit1 = equation.value : digit2 = equation.value;
-        equation.value = ""
+               (digit1 === undefined) ? digit1 = equation.value : digit2 = equation.value;
+               operator = "/";
+               equation.value = ""
             break;
+
         case "X": 
-        (digit1 === undefined) ? digit1 = equation.value : digit2 = equation.value;
-        equation.value = ""
+              (digit1 === undefined) ? digit1 = equation.value : digit2 = equation.value;
+              operator = "X"
+              equation.value = ""
             break;
+        case "AC":
+            equation.value = "";
+            digit1 = undefined;
+            digit2 = undefined;   
+            break;
+        case ".":
+            equation.value += event.target.value
+            elem.removeEventListener("click", equally);
+            break;
+             
         default:
             equation.value += event.target.value; 
     }
-    
-}
-function displayLog(event){
-    equation.value = event;
-}
+} 
+      
+} 
 
 button.forEach((elem) => {
     elem.addEventListener("click", equally)
